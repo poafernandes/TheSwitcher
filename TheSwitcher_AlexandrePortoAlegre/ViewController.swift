3d30b716d387be9d4546ff01f042cc7dd555a380
Alexandre Porto Alegre <alexandre.fernandes@contractor-mc1.com>
2023-01-30 13:13:16 -0300

    Initial commit

---
+++ TheSwitcher_AlexandrePortoAlegre/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSwitcher_AlexandrePortoAlegre
+//
+//  Created by Alexandre Porto Alegre Fernandes on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
